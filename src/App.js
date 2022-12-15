83f5aeee8cf327d84b6314ee3d96505de0fb7a91
vitek74 <vitek74@list.ru>
2022-12-15 13:04:45 +0300

    йцук

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import Content from './components/Content';
 import Footer from './components/Footer';
 import Header from './components/Header';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import '../App.css';
 import './Header.css';
+import clock from './clock';
+
 
 const Header = () => {
     return (
         <div class="header">
+            <clock />
             <h3>Header# "сделать обратный отчёт"</h3>
         </div>
     );
 }
+
+
+
+
 
 export default Header;
